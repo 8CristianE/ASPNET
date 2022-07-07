f8d86aefdaa9c5a28c13f47a2f7009b16d5f9d27
cristianespinosa7621@gmail.com <email@example.com>
2022-07-07 11:43:20 -0700

    View Product

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -24,7 +24,11 @@ namespace Testing.Controllers
             return View(products);
         }
 
-
+        public IActionResult ViewProduct(int id)
+        {
+            var product = repo.GetProduct(id);
+            return View(product);
+        }
     }
 }
 
